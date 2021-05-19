20e2c5b5e6c2026e9e0cdac425b82228dc1eb149
artom web <artomweb@gmail.com>
2021-05-19 20:41:53 +0100

    three way toggle

--- spotifyChart.js
+++ spotifyChart.js
@@ -1,180 +1,238 @@
 let data;
-let allDays = true;
 let myChart;
+let aggrData;
+let toggleState = 1;
+let desc = document.getElementById("spotify-desc");
 
-function aggregate(dat) {
-  dat = dat.map((d) => {
-    let day = moment(d.Date).format("dd");
-    return { dofw: day, Date: d.Date, Value: +d.Value };
-  });
+function spotifyToggle() {
+    switch (toggleState) {
+        case 0:
+            desc.innerHTML =
+                "How many songs have I listened to in the last two weeks";
+            updateChartBase(true);
+            break;
 
-  let totalAvgs = {};
+        case 1:
+            desc.innerHTML = "How many songs have I listened to all time";
+            updateChartBase(false);
+            break;
 
-  dat.forEach((o) => {
-    if (totalAvgs[o.dofw]) {
-      totalAvgs[o.dofw].total += o.Value;
-      totalAvgs[o.dofw].count += 1;
-    } else {
-      totalAvgs[o.dofw] = { total: o.Value, count: 1 };
+        case 2:
+            desc.innerHTML = "On which days do I listen to the most music";
+            updateAggregate();
+            break;
     }
-  });
-
-  Object.keys(totalAvgs).map((k, i) => {
-    totalAvgs[k].avg =
-      Math.round((totalAvgs[k].total / totalAvgs[k].count) * 100) / 100;
-  });
-
-  console.log(totalAvgs);
+    toggleState == 2 ? (toggleState = 0) : toggleState++;
 }
 
 async function newFetchSpotify() {
-  const response = await fetch(
-    "https://spreadsheets.google.com/feeds/list/1UYWe_3L4NiBU8_bwAbI1XTIRCToCDkOF44wUWVQ2gRE/1/public/full?alt=json"
-  );
+    const response = await fetch(
+        "https://spreadsheets.google.com/feeds/list/1UYWe_3L4NiBU8_bwAbI1XTIRCToCDkOF44wUWVQ2gRE/1/public/full?alt=json"
+    );
 
-  const json = await response.json();
+    const json = await response.json();
 
-  data = json.feed.entry.map((elt) => {
-    return {
-      Date: new Date(elt.gsx$date.$t),
-      Value: elt.gsx$value.$t,
-    };
-  });
+    data = json.feed.entry.map((elt) => {
+        return {
+            Date: new Date(elt.gsx$date.$t),
+            Value: elt.gsx$value.$t,
+        };
+    });
 
-  data = data.sort(function (a, b) {
-    return new Date(b.Date).getTime() - new Date(a.Date).getTime();
-  });
+    data = data.sort(function(a, b) {
+        return new Date(b.Date).getTime() - new Date(a.Date).getTime();
+    });
 
-  aggregate(data);
+    aggregate(data);
 
-  data = processData(data);
+    data = processData(data);
 
-  let firstData = data.dataTemp.slice(0, 14);
+    let firstData = data.rawData.slice(0, 14);
+    desc.innerHTML = "How many songs have I listened to in the last two weeks";
 
-  spotifyChart(firstData);
+    spotifyChart(firstData);
 }
 
 newFetchSpotify();
 
-function switchChart() {
-  let allData = data.dataTemp;
-  let labels = data.labels;
-  if (!allDays) {
-    allData = allData.slice(0, 14);
-    labels = labels.slice(0, 14);
-    allDays = true;
-  } else {
-    allDays = false;
-  }
+function updateAggregate() {
+    const { avgs, labels } = aggrData;
+    // console.log(avgs);
 
-  myChart.data.labels = labels;
+    myChart.data.labels = labels;
 
-  let newDataset = {
-    // tension: 0.3,
-    // borderColor: "black",
-    data: allData,
-    backgroundColor: "#81b29a",
-    // fill: false,
-  };
-  myChart.data.datasets = [newDataset];
+    let newDataset = {
+        // tension: 0.3,
+        // borderColor: "black",
+        data: avgs,
+        backgroundColor: "#81b29a",
+        // fill: false,
+    };
+    myChart.data.datasets = [newDataset];
 
-  //   console.log(myChart.data.datasets);
+    myChart.options.scales = {};
 
-  myChart.update();
+    //   console.log(myChart.data.datasets);
+
+    myChart.update();
+}
+
+function aggregate(dat) {
+    dat = dat.map((d) => {
+        let day = moment(d.Date).format("dd");
+        return { dofw: day, Date: d.Date, Value: +d.Value };
+    });
+
+    // console.log(dat);
+
+    let totalAvgs = _.chain(dat)
+        .groupBy("dofw")
+        .map((entries, day) => ({
+            dofw: day,
+            avg: Math.round(_.meanBy(entries, (entry) => entry.Value) * 100) / 100,
+        }))
+        .value();
+
+    totalAvgs = _.sortBy(totalAvgs, (o) => {
+        return moment(o.dofw, "dd").isoWeekday();
+    });
+
+    let labels = totalAvgs.map((val) => val.dofw);
+    let avgs = totalAvgs.map((val) => val.avg);
+
+    // console.log(avgs);
+
+    aggrData = { avgs, labels };
+}
+
+function updateChartBase(twoWeeks) {
+    let { rawData, labels } = data;
+    if (twoWeeks) {
+        rawData = rawData.slice(0, 14);
+        labels = labels.slice(0, 14);
+    }
+
+    myChart.data.labels = labels;
+
+    let newDataset = {
+        // tension: 0.3,
+        // borderColor: "black",
+        data: rawData,
+        backgroundColor: "#81b29a",
+        // fill: false,
+    };
+    myChart.data.datasets = [newDataset];
+
+    myChart.options.scales = {
+        xAxes: [{
+            type: "time",
+            time: {
+                unit: "day",
+                round: "day",
+                displayFormats: {
+                    day: "dd",
+                },
+            },
+        }, ],
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+            },
+        }, ],
+    };
+
+    //   console.log(myChart.data.datasets);
+
+    myChart.update();
 }
 
 function processData(dat) {
-  //   if (allData.length > days) {
-  //     allData = allData.slice(0, days);
-  //   }
+    let labels = dat.map(function(e) {
+        return new Date(e.Date);
+    });
 
-  let labels = dat.map(function (e) {
-    return new Date(e.Date);
-  });
+    let rawData = dat.map(function(e) {
+        return e.Value;
+    });
 
-  let dataTemp = dat.map(function (e) {
-    // return e.Duration;
-    return e.Value;
-  });
-
-  //   console.log(dataTemp);
-  return { dataTemp, labels };
+    return { rawData, labels };
 }
 
 function spotifyChart(allData) {
-  let currData = data.dataTemp.slice(0, 14);
-  let labels = data.labels.slice(0, 14);
+    let currData = data.rawData.slice(0, 14);
+    let labels = data.labels.slice(0, 14);
 
-  //   console.log(currData);
+    //   console.log(currData);
 
-  let ctx2 = document.getElementById("spotifyChart").getContext("2d");
-  myChart = new Chart(ctx2, {
-    type: "line",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          // tension: 0.3,
-          // borderColor: "black",
-          data: currData,
-          backgroundColor: "#81b29a",
-          // fill: false,
+    let ctx2 = document.getElementById("spotifyChart").getContext("2d");
+    myChart = new Chart(ctx2, {
+        type: "line",
+        data: {
+            labels: labels,
+            datasets: [{
+                // tension: 0.3,
+                // borderColor: "black",
+                data: currData,
+                backgroundColor: "#81b29a",
+                // fill: false,
+            }, ],
         },
-      ],
-    },
 
-    options: {
-      maintainAspectRatio: true,
-      responsive: true,
+        options: {
+            maintainAspectRatio: true,
+            responsive: true,
 
-      // layout: {
-      //     padding: {
-      //         left: 0,
-      //         right: 25,
-      //         top: 20,
-      //         bottom: 20,
-      //     },
-      // },
+            // layout: {
+            //     padding: {
+            //         left: 0,
+            //         right: 25,
+            //         top: 20,
+            //         bottom: 20,
+            //     },
+            // },
 
-      legend: {
-        display: false,
-      },
-      scales: {
-        xAxes: [
-          {
-            type: "time",
-            time: {
-              unit: "day",
-              round: "day",
-              displayFormats: {
-                day: "dd",
-              },
+            legend: {
+                display: false,
             },
-          },
-        ],
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
+            scales: {
+                xAxes: [{
+                    type: "time",
+                    time: {
+                        unit: "day",
+                        round: "day",
+                        displayFormats: {
+                            day: "dd",
+                        },
+                    },
+                }, ],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                }, ],
             },
-          },
-        ],
-      },
-      tooltips: {
-        callbacks: {
-          label: function (tooltipItem, data) {
-            let label = data.datasets[tooltipItem.datasetIndex].label || "";
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        let label = data.datasets[tooltipItem.datasetIndex].label || "";
 
-            if (label) {
-              label += ": ";
-            }
+                        if (label) {
+                            label += ": ";
+                        }
 
-            label += tooltipItem.yLabel + " songs";
-            return label;
-          },
+                        if (toggleState == 0) {
+                            label += tooltipItem.yLabel + " average";
+                        } else {
+                            label += tooltipItem.yLabel + " songs";
+                        }
+
+                        // label += tooltipItem.yLabel + (toggleState == 2 ? " average" : " songs");
+
+                        return label;
+                    },
+                },
+            },
         },
-      },
-    },
-  });
-  Chart.defaults.global.defaultFontColor = "#000";
+    });
+    Chart.defaults.global.defaultFontColor = "#000";
 }
