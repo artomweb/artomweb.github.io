3a825313bc849700d277d5d9bd233cec4671d479
Archie <artomweb@gmail.com>
2021-05-19 17:11:45 +0100

    aggregating

--- spotifyChart.js
+++ spotifyChart.js
@@ -2,6 +2,31 @@ let data;
 let allDays = true;
 let myChart;
 
+function aggregate(dat) {
+  dat = dat.map((d) => {
+    let day = moment(d.Date).format("dd");
+    return { dofw: day, Date: d.Date, Value: +d.Value };
+  });
+
+  let totalAvgs = {};
+
+  dat.forEach((o) => {
+    if (totalAvgs[o.dofw]) {
+      totalAvgs[o.dofw].total += o.Value;
+      totalAvgs[o.dofw].count += 1;
+    } else {
+      totalAvgs[o.dofw] = { total: o.Value, count: 1 };
+    }
+  });
+
+  Object.keys(totalAvgs).map((k, i) => {
+    totalAvgs[k].avg =
+      Math.round((totalAvgs[k].total / totalAvgs[k].count) * 100) / 100;
+  });
+
+  console.log(totalAvgs);
+}
+
 async function newFetchSpotify() {
   const response = await fetch(
     "https://spreadsheets.google.com/feeds/list/1UYWe_3L4NiBU8_bwAbI1XTIRCToCDkOF44wUWVQ2gRE/1/public/full?alt=json"
@@ -11,10 +36,16 @@ async function newFetchSpotify() {
 
   data = json.feed.entry.map((elt) => {
     return {
-      Date: elt.gsx$date.$t,
+      Date: new Date(elt.gsx$date.$t),
       Value: elt.gsx$value.$t,
     };
   });
+
+  data = data.sort(function (a, b) {
+    return new Date(b.Date).getTime() - new Date(a.Date).getTime();
+  });
+
+  aggregate(data);
 
   data = processData(data);
 
@@ -53,10 +84,6 @@ function switchChart() {
 }
 
 function processData(dat) {
-  dat.sort(function (a, b) {
-    return new Date(b.Date).getTime() - new Date(a.Date).getTime();
-  });
-
   //   if (allData.length > days) {
   //     allData = allData.slice(0, days);
   //   }
