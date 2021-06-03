da9a8939dbdec3dc1bf9fb96a7ac7117ffa3f603
artom web <artomweb@gmail.com>
2021-06-03 14:32:48 +0100

    live text

--- rppiSocket.js
+++ rppiSocket.js
@@ -4,3 +4,11 @@ socket.on("new data", function(msg) {
     // console.log(msg);
     document.getElementById("SensorReading").innerHTML = msg.LDR;
 });
+
+socket.on("connect", function() {
+    document.getElementById("liveText").style.color = "red";
+});
+
+socket.on("disconnect", function() {
+    document.getElementById("liveText").style.color = "black";
+});
