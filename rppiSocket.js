7da215682c74271d8b060bd285914d11fca82450
artom web <artomweb@gmail.com>
2021-06-03 17:52:06 +0100

    degree

--- rppiSocket.js
+++ rppiSocket.js
@@ -3,9 +3,11 @@ let socket = io("https://rppi.artomweb.com", { reconnectionDelay: 500 });
 socket.on("new data", function(msg) {
     // console.log(msg);
     document.getElementById("temperatureReading").innerHTML =
-        "Temperature: " + (Math.round(msg.Temperature * 100) / 100).toFixed(2);
+        "Temperature: " +
+        (Math.round(msg.Temperature * 100) / 100).toFixed(2) +
+        String.fromCharCode(176);
     document.getElementById("pressureReading").innerHTML =
-        "Pressure: " + (Math.round(msg.Pressure * 100) / 100).toFixed(2);
+        "Pressure: " + (Math.round(msg.Pressure * 100) / 100).toFixed(2) + " hPa";
 });
 
 socket.on("connect", function() {
