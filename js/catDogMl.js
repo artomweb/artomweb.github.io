1ee8c5240e39183d1c8196848b958de4d0ba5797
artom web <artomweb@gmail.com>
2022-01-11 21:54:47 +0000

    working

--- js/catDogMl.js
+++ js/catDogMl.js
@@ -3,14 +3,34 @@ function getNewImage() {
         .then((res) => res.json())
         .then((out) => insertCatDog(out))
         .catch((err) => {
-            console.log("Could not get image data");
+            console.log("Could not get image data", err);
         });
 }
 
 getNewImage();
 
+let catDogImageRef = document.getElementById("catDogImage");
+
 function insertCatDog(data) {
-    let catDogImageRef = document.getElementById("catDogImage");
     catDogImageRef.src = "https://rppi.artomweb.com" + data.imageLink;
+    catDogImageRef.setAttribute("imageURI", data.imageURI);
     console.log(data);
+}
+
+function catDogChoice(cORd) {
+    getNewImage();
+    let imageURI = catDogImageRef.getAttribute("imageURI");
+    console.log(cORd);
+    fetch(
+            "https://rppi.artomweb.com/ml/label?" +
+            new URLSearchParams({
+                imageURI: imageURI,
+                catORdog: cORd,
+            }), { method: "POST" }
+        )
+        .then((res) => res.text())
+        .then((out) => console.log(out))
+        .catch((err) => {
+            console.log("Could not send query", err);
+        });
 }
