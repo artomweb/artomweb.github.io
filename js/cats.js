0273eca4eb28fd73c45d772d52cf6d33c453d4f0
artom web <artomweb@gmail.com>
2022-05-14 16:58:33 +0100

    standing cat card

---
+++ js/cats.js
@@ -0,0 +1,11 @@
+const NUMCATS = 67;
+
+function loadCat() {
+    let catInt = Math.floor(Math.random() * NUMCATS) + 1;
+    // console.log(catInt);
+    let catImg = document.getElementById("catImage");
+
+    catImg.src = "standingCats/" + catInt + ".jpg";
+}
+
+loadCat();
