2461d2d693ee1648150f228190a22531cba9716f
artom web <artomweb@gmail.com>
2022-07-11 19:12:18 +0100

    shuffle cats

--- js/cats.js
+++ js/cats.js
@@ -1,11 +1,23 @@
-const NUMCATS = 92;
+let imageNumbers = Array(92)
+    .fill()
+    .map((v, i) => i + 1);
+
+imageNumbers = shuffle(imageNumbers);
+
+let currentImageIndex = 90;
+
+const MAX_CATS = 92;
 
 function loadCat() {
-    let catInt = Math.floor(Math.random() * NUMCATS) + 1;
-    // console.log(catInt);
     let catImg = document.getElementById("catImage");
 
-    catImg.src = "standingCats/" + catInt + ".jpg";
+    catImg.src = "standingCats/" + imageNumbers[currentImageIndex] + ".jpg";
+
+    if (currentImageIndex >= MAX_CATS - 1) {
+        currentImageIndex = 0;
+    } else {
+        currentImageIndex++;
+    }
 }
 
 loadCat();
