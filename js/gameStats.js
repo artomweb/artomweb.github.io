6e195b4ea9ffbbb9653f658e3883a660db1f2a82
artom web <artomweb@gmail.com>
2022-10-15 19:16:11 +0100

    only game stats

--- js/gameStats.js
+++ js/gameStats.js
@@ -1,12 +1,13 @@
-function fetchGames() {
-    fetch("https://rppi.artomweb.com/cache/game")
-        .then((res) => res.json())
-        .then((out) => gamesMain(out))
-        .catch((err) => {
-            console.log("failed to fetch from cache, games", err);
-            let moreStatsCard = document.getElementById("moreStatsCard");
-            moreStatsCard.style.display = "none";
-        });
+async function fetchGames() {
+    Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vRd3YjXEoMgwyej-Y6uIwHtZUlWtBYjB6nBoexbjVxreBsbDPaY9cPsr8QnUuY-WthpVfS3gNbIXidV/pub?output=csv", {
+        download: true,
+        complete: function(results) {
+            gamesMain(results.data);
+        },
+        error: function(error) {
+            console.log("failed to fetch from cache, games");
+        },
+    });
 }
 
 fetchGames();
@@ -15,10 +16,14 @@ function gamesMain(data) {
     let timeCSGO = document.getElementById("timeCSGO");
     let timeBF5 = document.getElementById("timeBF5");
     let timeRDR2 = document.getElementById("timeRDR2");
+    let dataJson = {};
+    data.forEach((elem) => {
+        dataJson[elem[0]] = elem[1];
+    });
 
-    let CSGOMsg = createTimeMessage(data[730]);
-    let BF5Msg = createTimeMessage(data[1238810]);
-    let RDR2Msg = createTimeMessage(data[1174180]);
+    let CSGOMsg = createTimeMessage(dataJson[730] * 60);
+    let BF5Msg = createTimeMessage(dataJson[1238810] * 60);
+    let RDR2Msg = createTimeMessage(dataJson[1174180] * 60);
 
     timeCSGO.innerHTML = CSGOMsg;
     timeBF5.innerHTML = BF5Msg;
