552997ea87af4c8602d74f894c53cd0c19fd48eb
artom web <artomweb@gmail.com>
2023-03-21 16:05:34 +0000

    added line break so that spotify box does not move around

--- js/spotifyChart.js
+++ js/spotifyChart.js
@@ -34,7 +34,7 @@ function switchSpotifyDots() {
       desc.innerHTML = "How many songs have I listened to in the last two weeks";
       break;
     case 2:
-      desc.innerHTML = "Each week, for the last year";
+      desc.innerHTML = "Each week, for the last year <br><br>";
       break;
   }
   circles.forEach((c) => (c.id.slice(-1) == toggleState ? (c.style.fill = "black") : (c.style.fill = "none")));
