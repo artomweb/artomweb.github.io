8c650c6d80bcc2dd36775cf602937f623385a8d0
artom web <artomweb@gmail.com>
2024-02-10 23:18:41 +0000

    fixed cat aspect

--- js/cats.js
+++ js/cats.js
@@ -1,9 +1,9 @@
-const MAX_CATS = 127;
+const MAX_CATS = 59;
 
 // Create an array of integers
 let imageNumbers = Array(MAX_CATS)
-    .fill()
-    .map((v, i) => i + 1);
+  .fill()
+  .map((v, i) => i + 1);
 
 imageNumbers = shuffle(imageNumbers);
 
@@ -11,20 +11,20 @@ let currentImageIndex = 0;
 
 // When the button is clicked, change the image source to the current index
 function loadCat() {
-    let catImg = document.getElementById("catImage");
-    let imageCache = document.getElementById("imageCache");
+  let catImg = document.getElementById("catImage");
+  let imageCache = document.getElementById("imageCache");
 
-    catImg.src = "standingCats/" + imageNumbers[currentImageIndex] + ".jpg";
+  catImg.src = "standingCats/" + imageNumbers[currentImageIndex] + ".jpg";
 
-    // When on last cat, set index to 0 and reshuffle
-    if (currentImageIndex >= MAX_CATS - 1) {
-        currentImageIndex = 0;
-        imageNumbers = shuffle(imageNumbers);
-    } else {
-        // Cache the next image
-        imageCache.src = "standingCats/" + imageNumbers[currentImageIndex + 1] + ".jpg";
-        currentImageIndex++;
-    }
+  // When on last cat, set index to 0 and reshuffle
+  if (currentImageIndex >= MAX_CATS - 1) {
+    currentImageIndex = 0;
+    imageNumbers = shuffle(imageNumbers);
+  } else {
+    // Cache the next image
+    imageCache.src = "standingCats/" + imageNumbers[currentImageIndex + 1] + ".jpg";
+    currentImageIndex++;
+  }
 }
 
 loadCat();
