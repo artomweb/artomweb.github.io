0aff95df0718d3aa15d10ec195c48aaa180d312d
artom web <artomweb@gmail.com>
2025-11-11 19:43:18 +0000

    update voltage range and decimal points

--- src/solar.js
+++ src/solar.js
@@ -13,32 +13,21 @@ export default function parseSolar(data) {
 }
 
 function showSolarData(data) {
-  const batteryMinV = 14; // 22.4V
-  const batteryMaxV = 15.2; // 28.35V
+  const batteryMinV = 12.4;
+  const batteryMaxV = 16.2;
   const batteryV = data.V / 1000;
 
-  const batteryPercentage = Math.max(
-    ((batteryV - batteryMinV) / (batteryMaxV - batteryMinV)) * 100,
-    0
-  );
+  const batteryPercentage = Math.max(((batteryV - batteryMinV) / (batteryMaxV - batteryMinV)) * 100, 0);
 
-  document.getElementById("solarV").innerHTML =
-    batteryPercentage.toFixed(2) + " %";
-  document.getElementById("solarI").innerHTML =
-    (data.I > 0 ? "+" : "") + data.I + " mA";
+  document.getElementById("solarV").innerHTML = batteryPercentage.toFixed(1) + " %";
+  document.getElementById("solarI").innerHTML = (data.I > 0 ? "+" : "") + data.I + " mA";
   document.getElementById("solarCPU").innerHTML = data.serverStats.cpu + " %";
   document.getElementById("solarPower").innerHTML = data.PPV + " W";
 
   if (data.I > 0) {
-    document.getElementById("solarState").innerHTML =
-      "Charging at " +
-      Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(2)) +
-      " W";
+    document.getElementById("solarState").innerHTML = "Charging at " + Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(1)) + " W";
   } else {
-    document.getElementById("solarState").innerHTML =
-      "Discharging at " +
-      Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(2)) +
-      " W";
+    document.getElementById("solarState").innerHTML = "Discharging at " + Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(1)) + " W";
   }
   //   document.getElementById("lastSolar").innerHTML = dateOfLastReading;
 
