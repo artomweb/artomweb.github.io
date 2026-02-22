a6ae2939fec5ec093d91b8cbdf2a6baf24a68565
Archie <artomweb@gmail.com>
2026-02-22 16:49:26 +0000

    add uptime

--- src/solar.js
+++ src/solar.js
@@ -11,7 +11,6 @@ export default function parseSolar(data) {
     }
   }
 }
-
 function showSolarData(data) {
   const batteryMinV = 12.4;
   const batteryMaxV = 16.2;
@@ -19,17 +18,26 @@ function showSolarData(data) {
 
   const batteryPercentage = Math.max(((batteryV - batteryMinV) / (batteryMaxV - batteryMinV)) * 100, 0);
 
+  // Existing fields
   document.getElementById("solarV").innerHTML = batteryPercentage.toFixed(1) + " %";
   document.getElementById("solarI").innerHTML = (data.I > 0 ? "+" : "") + data.I + " mA";
   document.getElementById("solarCPU").innerHTML = data.serverStats.cpu + " %";
   document.getElementById("solarPower").innerHTML = data.PPV + " W";
 
-  if (data.I > 0) {
-    document.getElementById("solarState").innerHTML = "Charging at " + Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(1)) + " W";
-  } else {
-    document.getElementById("solarState").innerHTML = "Discharging at " + Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(1)) + " W";
-  }
-  //   document.getElementById("lastSolar").innerHTML = dateOfLastReading;
+  // State logic
+  const powerW = Math.abs(((data.V / 1000) * (data.I / 1000)).toFixed(1));
+  document.getElementById("solarState").innerHTML = (data.I > 0 ? "Charging at " : "Discharging at ") + powerW + " W";
 
-  //   drawSolarChart(data.dailyData); // Array of daily entries
+  if (data.uptime) {
+    const uptime = data.uptime;
+
+    // 7 Day Uptime
+    const uptime7Str = `${uptime.uptime_7d_pct.toFixed(0)} %` + (uptime.downtime_7d_hours > 0 ? ` (${uptime.downtime_7d_hours} hours down)` : "");
+    document.getElementById("solarUptime7").innerHTML = uptime7Str;
+
+    // 30 Day Uptime
+    const uptime30Str =
+      `${uptime.uptime_30d_pct.toFixed(0)} %` + (uptime.downtime_30d_hours > 0 ? ` (${uptime.downtime_30d_hours.toFixed(0)} hours down)` : "");
+    document.getElementById("solarUptime30").innerHTML = uptime30Str;
+  }
 }
